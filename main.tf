6d024b1378e5b04bb002e57b9077421c63134af5
Stéphane de Wit <contact@stephanedewit.be>
2021-09-01 23:44:07 +0200

    Create a simple server

--- main.tf
+++ main.tf
@@ -11,3 +11,8 @@ provider "scaleway" {
   zone            = "fr-par-1"
   region          = "fr-par"
 }
+
+resource "scaleway_instance_server" "web" {
+	type = "DEV1-S"
+	image = "debian_bullseye"
+}
