d8bd74647576f68f4b152c0385411cc381179ec0
Stéphane de Wit <contact@stephanedewit.be>
2021-09-17 17:52:36 +0200

    Create DynamoDB table
    For Terraform state locks.

--- provisioning/bootstrap/s3-backend.tf
+++ provisioning/bootstrap/s3-backend.tf
@@ -6,3 +6,14 @@ resource "aws_s3_bucket" "state-bucket" {
     enabled = true
   }
 }
+
+resource "aws_dynamodb_table" "state-table" {
+  name         = "TerraformStateLocks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
