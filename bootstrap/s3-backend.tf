dad6d0c006ae01fd5f9bb1f9a6aef93bb531639f
Stéphane de Wit <contact@stephanedewit.be>
2021-09-18 23:08:26 +0200

    Create AWS policy for Terraform state

--- bootstrap/s3-backend.tf
+++ bootstrap/s3-backend.tf
@@ -25,3 +25,37 @@ resource "aws_dynamodb_table" "state-table" {
     prevent_destroy = true
   }
 }
+
+resource "aws_iam_policy" "policy" {
+  name = "TerraformState"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+        ]
+        Resource = aws_s3_bucket.state-bucket.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+        ]
+        Resource = "${aws_s3_bucket.state-bucket.arn}/states/*.tfstate"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem",
+        ]
+        Resource = aws_dynamodb_table.state-table.arn
+      },
+    ]
+  })
+}
