30aa9b8848dbeb506af20ae92d4c9a3420e0df70
Stéphane de Wit <contact@stephanedewit.be>
2021-10-25 17:57:36 +0200

    Deploy tools-project services

---
+++ services/tools-hash.tf
@@ -0,0 +1,13 @@
+resource "docker_image" "tools_hash" {
+  name         = "28solutions/tools-hash:0.1.3"
+  keep_locally = true
+}
+
+resource "docker_container" "tools_hash" {
+  name  = "tools_hash"
+  image = docker_image.tools_hash.latest
+
+  ports {
+    internal = 8000
+  }
+}
---
+++ services/tools-remote-ip.tf
@@ -0,0 +1,13 @@
+resource "docker_image" "tools_remote_ip" {
+  name         = "28solutions/tools-remote-ip:0.1.2"
+  keep_locally = true
+}
+
+resource "docker_container" "tools_remote_ip" {
+  name  = "tools_remote_ip"
+  image = docker_image.tools_remote_ip.latest
+
+  ports {
+    internal = 8000
+  }
+}
