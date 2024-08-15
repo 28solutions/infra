345f21c0dd5b772d25e1015012a160e2f879d30a
Stéphane de Wit <contact@stephanedewit.be>
2024-08-16 01:53:55 +0200

    Generate certificate for twentyeight.solutions

--- provisioning/acme.tf
+++ provisioning/acme.tf
@@ -3,10 +3,21 @@ variable "acme_email_address" {
   description = "ACME account email address"
 }
 
-variable "domain_name" {
-  type        = string
+variable "domain_names" {
+  type        = map(list(string))
   description = "Common name for TLS certificate"
-  default     = "tools.28.solutions"
+  default = {
+    tools_28s = [
+      "tools.28.solutions",
+      "www.tools.28.solutions"
+    ]
+    www_28s = [
+      "twentyeight.solutions",
+      "www.twentyeight.solutions",
+      "28.solutions",
+      "www.28.solutions"
+    ]
+  }
 }
 
 resource "tls_private_key" "acme_private_key" {
@@ -19,23 +30,16 @@ resource "acme_registration" "acme_registration" {
 }
 
 module "certificates" {
+  for_each = var.domain_names
+
   source = "./acme-certificate"
 
   account_key_pem = tls_private_key.acme_private_key.private_key_pem
-  common_name     = var.domain_name
-  dns_names       = [var.domain_name]
+  common_name     = each.value[0]
+  dns_names       = each.value
 }
 
-output "domain_name" {
-  value = module.certificates.common_name
-}
-
-output "certificate" {
-  value     = module.certificates.certificate
-  sensitive = true # not really sensitive, but it floods the console
-}
-
-output "certificate_private_key" {
-  value     = module.certificates.private_key
+output "certificates" {
+  value     = module.certificates
   sensitive = true
 }
