91cd4c5c0e944cafc4271c7eb8899abe54a9161d
Stéphane de Wit <contact@stephanedewit.be>
2024-08-17 10:48:07 +0200

    Upgrade Docker image

--- services/www.tf
+++ services/www.tf
@@ -5,9 +5,9 @@ resource "docker_network" "www" {
 module "www_service" {
   source = "./docker-http-service"
 
-  image         = "28solutions/www:0.1.0"
+  image         = "28solutions/www:0.2.0"
   container     = "www_28s"
-  internal_port = 80
+  internal_port = 8080
   network       = docker_network.www.name
 
   hosts = [
