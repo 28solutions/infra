1d278ec2623ff2f2359b932c41f70ebb21c9876a
Stéphane de Wit <contact@stephanedewit.be>
2024-10-29 21:00:37 +0100

    Move remote states into their own file

---
+++ services/remote-states.tf
@@ -0,0 +1,11 @@
+data "terraform_remote_state" "storage" {
+  backend = "s3"
+
+  config = {
+    bucket         = var.bucket
+    key            = "states/storage.tfstate"
+    region         = var.region
+    profile        = var.profile
+    dynamodb_table = var.dynamodb_table
+  }
+}
