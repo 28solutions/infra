78de50a6bc6633de3fda290eba10aa335094a869
Stéphane de Wit <contact@stephanedewit.be>
2024-10-30 13:30:21 +0100

    Use bucket index module for 'downloads' bucket

--- storage/downloads-bucket.tf
+++ storage/downloads-bucket.tf
@@ -2,10 +2,17 @@ resource "scaleway_object_bucket" "downloads" {
   name = "sdw-downloads"
 }
 
+module "downloads_bucket_index" {
+  source = "./bucket-index"
+
+  title = "Downloads"
+  icon  = "download"
+}
+
 resource "scaleway_object" "downloads_index" {
   bucket = scaleway_object_bucket.downloads.id
   key    = "index.html"
 
-  file = "downloads/index.html"
-  hash = filemd5("downloads/index.html")
+  content = module.downloads_bucket_index.content
+  hash    = module.downloads_bucket_index.hash
 }
