d615bec2df1748835bce3093642bcf2bd13c9cd8
Stéphane de Wit <contact@stephanedewit.be>
2024-10-31 20:00:39 +0100

    Add 'not after' field to ACME certificate output

--- provisioning/acme-certificate/outputs.tf
+++ provisioning/acme-certificate/outputs.tf
@@ -6,6 +6,10 @@ output "certificate" {
   value = "${acme_certificate.certificate.certificate_pem}${acme_certificate.certificate.issuer_pem}"
 }
 
+output "not_after" {
+  value = acme_certificate.certificate.certificate_not_after
+}
+
 output "private_key" {
   value     = tls_private_key.cert_private_key.private_key_pem
   sensitive = true
