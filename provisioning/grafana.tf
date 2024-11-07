841cb7dcd1a0012edfdce50a6c514f63f279e2f5
Stéphane de Wit <contact@stephanedewit.be>
2024-11-08 00:49:30 +0100

    Create Grafana user

--- provisioning/grafana.tf
+++ provisioning/grafana.tf
@@ -5,3 +5,26 @@ resource "scaleway_cockpit" "cockpit" {
 
   plan = "free"
 }
+
+resource "scaleway_cockpit_grafana_user" "user" {
+  project_id = data.scaleway_account_project.project.project_id
+
+  login = "stephdewit"
+  role  = "editor"
+}
+
+data "onepassword_vault" "iac_vault" {
+  name = "IaC"
+}
+
+resource "onepassword_item" "grafana_login" {
+  vault = data.onepassword_vault.iac_vault.uuid
+
+  title    = "Scaleway Grafana | www"
+  category = "login"
+
+  username = scaleway_cockpit_grafana_user.user.login
+  password = scaleway_cockpit_grafana_user.user.password
+
+  url = "https://${scaleway_cockpit_grafana_user.user.project_id}.dashboard.cockpit.fr-par.scw.cloud"
+}
