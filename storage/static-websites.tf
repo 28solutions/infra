07eeb7a4fc6d86f2c604b712ce19ea14539fcd6c
Stéphane de Wit <contact@stephanedewit.be>
2024-11-08 17:54:30 +0100

    Create a map of static websites

--- storage/static-websites.tf
+++ storage/static-websites.tf
@@ -1,45 +1,51 @@
-module "pki_website" {
+locals {
+  websites = {
+    pki = {
+      provider   = "aws"
+      bucket     = "sdw-pki"
+      versioning = true
+      title      = "PKI"
+      icon       = "key"
+    }
+    downloads_sdw = {
+      provider   = "scw"
+      bucket     = "sdw-downloads"
+      versioning = false
+      title      = "Downloads"
+      icon       = "download"
+    }
+    downloads_28s = {
+      provider   = "scw"
+      bucket     = "28s-downloads"
+      versioning = false
+      title      = "Downloads"
+      icon       = "download"
+    }
+  }
+}
+
+module "website" {
+  for_each = local.websites
+
   source = "./static-website"
 
-  cloud_provider = "aws"
+  cloud_provider = each.value.provider
 
-  bucket            = "sdw-pki"
-  bucket_versioning = true
+  bucket            = each.value.bucket
+  bucket_versioning = each.value.versioning
 
-  title = "PKI"
-  icon  = "key"
+  title = each.value.title
+  icon  = each.value.icon
 }
 
 output "pki_domain_name" {
-  value = module.pki_website.domain_name
-}
-
-module "downloads_sdw_website" {
-  source = "./static-website"
-
-  cloud_provider = "scw"
-
-  bucket = "sdw-downloads"
-
-  title = "Downloads"
-  icon  = "download"
+  value = module.website["pki"].domain_name
 }
 
 output "downloads_sdw_domain_name" {
-  value = module.downloads_sdw_website.domain_name
-}
-
-module "downloads_28s_website" {
-  source = "./static-website"
-
-  cloud_provider = "scw"
-
-  bucket = "28s-downloads"
-
-  title = "Downloads"
-  icon  = "download"
+  value = module.website["downloads_sdw"].domain_name
 }
 
 output "downloads_28s_domain_name" {
-  value = module.downloads_28s_website.domain_name
+  value = module.website["downloads_28s"].domain_name
 }
