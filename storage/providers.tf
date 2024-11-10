01e21e29e309df7a19a03b4cbfbd1034811372d5
Stéphane de Wit <contact@stephanedewit.be>
2024-11-10 18:19:12 +0100

    Get Scaleway (storage) credentials from 1Password

--- storage/providers.tf
+++ storage/providers.tf
@@ -16,10 +16,25 @@ terraform {
   }
 }
 
+data "onepassword_item" "scaleway_storage" {
+  vault = data.onepassword_vault.iac_vault.uuid
+  title = "Scaleway - storage"
+}
+
+locals {
+  op_project_section = [for s in data.onepassword_item.scaleway_storage.section : s if s.label == "Project"][0]
+  op_project_fields  = { for f in local.op_project_section.field : f.label => f.value }
+}
+
 provider "scaleway" {
-  profile = "storage"
-  zone    = "fr-par-1"
-  region  = "fr-par"
+  access_key = data.onepassword_item.scaleway_storage.username
+  secret_key = data.onepassword_item.scaleway_storage.credential
+
+  organization_id = local.op_project_fields["Organization ID"]
+  project_id      = local.op_project_fields["Project ID"]
+
+  region = "fr-par"
+  zone   = "fr-par-1"
 }
 
 data "onepassword_item" "aws_sdw" {
