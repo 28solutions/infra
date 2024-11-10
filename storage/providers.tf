7740c65477a1bfa38489066ad3b9e78ffaee50cd
Stéphane de Wit <contact@stephanedewit.be>
2024-11-10 17:26:52 +0100

    Get AWS credentials from 1Password

--- storage/providers.tf
+++ storage/providers.tf
@@ -22,7 +22,13 @@ provider "scaleway" {
   region  = "fr-par"
 }
 
+data "onepassword_item" "aws_sdw" {
+  vault = data.onepassword_vault.iac_vault.uuid
+  title = "Amazon Web Services - SDW terraform"
+}
+
 provider "aws" {
-  profile = "sdwbe"
-  region  = "eu-central-1"
+  access_key = data.onepassword_item.aws_sdw.username
+  secret_key = data.onepassword_item.aws_sdw.credential
+  region     = "eu-central-1"
 }
