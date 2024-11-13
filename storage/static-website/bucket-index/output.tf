18d4fc497402e165432068fd2443d756162b53f6
Stéphane de Wit <contact@stephanedewit.be>
2024-11-13 02:17:15 +0100

    Rename folder

--- storage/static-website/bucket-index/output.tf
+++ storage/static-website/bucket-index/output.tf
@@ -1,6 +1,6 @@
 locals {
   content = templatefile(
-    "${path.module}/public_html/index.html",
+    "${path.module}/public-html/index.html",
     {
       title = var.title
       icon  = var.icon
