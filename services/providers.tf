d806ff4111a292a71edf524cbb7807e825244781
Stéphane de Wit <contact@stephanedewit.be>
2024-11-16 14:35:41 +0100

    Use user certificate (if present) for SSH user

--- services/providers.tf
+++ services/providers.tf
@@ -16,9 +16,12 @@ terraform {
 locals {
   ssh_host = data.terraform_remote_state.provisioning.outputs.web_server_hostname
   ssh_port = data.terraform_remote_state.provisioning.outputs.web_server_ssh_port
+
+  ssh_cert_file = "/ssh/terraform-cert.pub"
+  ssh_cert_opt  = fileexists(local.ssh_cert_file) ? ["-o", "CertificateFile=${local.ssh_cert_file}"] : []
 }
 
 provider "docker" {
   host     = "ssh://stephdewit@${local.ssh_host}:${local.ssh_port}"
-  ssh_opts = ["-o", "ControlMaster=no"]
+  ssh_opts = concat(["-o", "ControlMaster=no"], local.ssh_cert_opt)
 }
