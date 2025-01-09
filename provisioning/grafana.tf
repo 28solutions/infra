51c90637a874a88cff3816a178562cdd5ba57632
Stéphane de Wit <contact@stephanedewit.be>
2025-01-09 18:58:23 +0100

    Remove deprecated 'plan' attribute

--- provisioning/grafana.tf
+++ provisioning/grafana.tf
@@ -2,8 +2,6 @@ data "scaleway_account_project" "project" {}
 
 resource "scaleway_cockpit" "cockpit" {
   project_id = data.scaleway_account_project.project.project_id
-
-  plan = "free"
 }
 
 resource "scaleway_cockpit_token" "traefik" {
