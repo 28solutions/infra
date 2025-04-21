cd1219bdfd785e57d6daa48ea2122fb15cf2f578
Stéphane de Wit <contact@stephanedewit.be>
2025-04-21 12:03:33 +0200

    Remove Terraform state DynamoDB table

--- bootstrap/s3-backend.tf
+++ bootstrap/s3-backend.tf
@@ -43,21 +43,6 @@ resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
   }
 }
 
-resource "aws_dynamodb_table" "state-table" {
-  name         = "TerraformStateLocks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
 resource "aws_iam_policy" "policy" {
   name = "TerraformState"
 
