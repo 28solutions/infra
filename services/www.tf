c78fbf63d69c517228cd3705ae6e13571467cb56
Stéphane de Wit <contact@stephanedewit.be>
2025-12-04 19:53:26 +0100

    Upgrade 28s website

--- services/www.tf
+++ services/www.tf
@@ -5,7 +5,7 @@ resource "docker_network" "www" {
 module "www_28s_service" {
   source = "./docker-http-service"
 
-  image         = "28solutions/www:2.0.0"
+  image         = "28solutions/www:2.0.1"
   container     = "www-28s"
   internal_port = 8080
   network       = docker_network.www.name
