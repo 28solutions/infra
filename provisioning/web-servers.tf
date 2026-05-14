400e7f41f82e62cdbfd40a2ebac949ef8db28854
Stéphane de Wit <contact@stephanedewit.be>
2026-05-14 18:22:55 +0200

    Add web server instance name

--- provisioning/web-servers.tf
+++ provisioning/web-servers.tf
@@ -101,6 +101,7 @@ resource "scaleway_instance_ip" "web_public_ipv6" {
 resource "scaleway_instance_server" "web" {
   type              = "DEV1-S"
   image             = "debian_trixie"
+  name              = "kenny"
   security_group_id = scaleway_instance_security_group.www.id
 
   ip_ids = [
