71ab84e47e6e7a9ff53225b689781e8dc13a8b24
Stéphane de Wit <contact@stephanedewit.be>
2026-06-02 18:10:39 +0200

    Add updown checks for websites

--- services/updown.tf
+++ services/updown.tf
@@ -4,10 +4,15 @@ locals {
 
   updown_domains_section = [for s in data.onepassword_item.updown.section : s if s.label == "Domains"][0]
   updown_1p_domains      = { for f in local.updown_domains_section.field : f.label => nonsensitive(f.value) }
+
+  updown_domains = {
+    "28s" = module.www_28s_service.main_host
+    sdw   = module.www_sdw_service.main_host
+  }
 }
 
 resource "updown_check" "checks" {
-  for_each = local.updown_1p_domains
+  for_each = merge(local.updown_domains, local.updown_1p_domains)
 
   url    = "https://${each.value}"
   period = 300
